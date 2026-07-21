fedf8ec7f13319cb458599d339dcbba8d79c3202
Nguyễn An <annguyen04@hotmail.com>
2026-07-21 15:58:09 +0700

    fix: trap startup crash and show Win32 MessageBox on Windows

--- AnvViberManager/Program.cs
+++ AnvViberManager/Program.cs
@@ -9,8 +9,32 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                // Gọi Win32 MessageBox để thông báo lỗi thay vì silent crash
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "powershell",
+                    Arguments = $"-Command \"[System.Windows.Forms.MessageBox]::Show('An error occurred during startup: {ex.Message.Replace("'", "''")}\\n\\nDetails: {ex.ToString().Replace("'", "''")}', 'Startup Crash Error')\"",
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                });
+            }
+            else
+            {
+                Console.WriteLine("CRASH EXCEPTION: " + ex);
+            }
+        }
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
